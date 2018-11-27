2a5d91c2c224be732f1ae9edb6025b078354ec5b
Simon Balean <simonbalean@gmail.com>
2018-11-27 10:11:17 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,10 +27,13 @@ def monopoly_with_third_tier
 	  {railroads: {
       pieces: 4, 
 	    names: {
-	      
+	      reading_railroad: {}
 	    },
 	    rent_in_dollars: {
 	      one_piece_owned: 25
+	      two_pieces_owned: 50
+	      three_pieces_owned: 100
+	      four_pieces_owned: 200
 	    }
 	  }
 	}
