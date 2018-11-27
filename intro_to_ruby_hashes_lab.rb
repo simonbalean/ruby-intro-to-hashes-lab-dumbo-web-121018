5e9b6b4a42f730047309f2b4350aac01d041cba6
Simon Balean <simonbalean@gmail.com>
2018-11-27 10:06:44 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,9 +13,11 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {railroads:{
-	  pieces: 4, 
-	  names:{}
+	monopoly = 
+	  {railroads: {
+      pieces: 4, 
+	    names: {},
+	    rent_in_dollars: {}
 	  }
 	}
 end
